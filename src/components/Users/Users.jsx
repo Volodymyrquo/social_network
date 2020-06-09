217b66a10fde9d37accb8b76a346ea1da6ca087f
Volodymyr <volodymyrquo@gmail.com>
2020-06-09 06:26:37 +0300

    lesson 51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png"
 
 const  Users = (props) => {
-    if(props.users.length === 0) {
-        props.setUsers(
-            [
-            {id: 1,photoUrl: 'https://pbs.twimg.com/media/DMGGo4wVAAEIray.jpg',followed: false, fullName: 'Dmitriy',  status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus' }},
-            {id: 2,photoUrl: 'https://socialniesety.ru/files/images/components/articles_journal/originals/instagram/234/avatarka_dlya_instagram_primery_15.jpg',followed: true, fullName: 'Bob' ,status: 'I am a boss too', location: {city: 'New York', country: 'USA' }},
-            {id: 3, photoUrl: 'https://socialniesety.ru/files/images/components/articles_journal/originals/instagram/422/kak_sdelat_artavatarku_v_instagram_3.jpg',followed: false, fullName: 'Vlada' ,status: 'I am a boss too', location: {city: 'Kyiv', country: 'Ukraine' }}
 
-        ]
-        )
+
+
+    if(props.users.length === 0) {
+
+           axios.get(`https://social-network.samuraijs.com/api/1.0/users`)
+               .then(response => {
+                   props.setUsers(response.data.items)
+               })
+
+
     }
+
 
 return (
     <div className={styles.user}>
        { props.users.map(u => <div key={u.id}>
 <span>
     <div>
-        <img src={u.photoUrl} className={styles.userPhoto}/>
+        <img src={u.photos.small?u.photos.small:userPhoto} className={styles.userPhoto}/>
     </div>
     <div>
             {u.followed ?
@@ -28,12 +33,12 @@ return (
 </span>
 <span>
     <span>
-        <div>{u.fullName}</div>
+        <div>{u.name}</div>
         <div>{u.status}</div>
     </span>
     <span>
-        <div>{u.location.country}</div>
-        <div>{u.location.city}</div>
+        <div>{"location.country"}</div>
+        <div>{"location.city"}</div>
     </span>
 </span>
 
