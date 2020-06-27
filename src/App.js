9e26e36a333ffc68c56b5a445bcc5ca8d9c3b230
Volodymyr Basok <volodymyrquo@gmail.com>
2020-06-27 09:41:52 +0300

    Lesson 95 git-hab pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import News from "./components/news/News";
 import Music from "./components/music/Music";
 import Settings from "./components/settings/Settings";
-import DialogsContainer from "./components/dialogs/DialogsContainer";
 import UsersContainer from "./components/users/UsersContainer";
-import ProfileContainer from "./components/profile/ProfileContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
 import Login from "./components/login/Login";
 import {connect, Provider} from "react-redux";
@@ -15,6 +13,11 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/preloader/Preloader";
 import store from "./redux/redux-store";
+import {withSuspense} from "./hoc/withSuspense";
+
+const  DialogsContainer = React.lazy(() => import("./components/dialogs/DialogsContainer"));
+const  ProfileContainer = React.lazy(() => import("./components/profile/ProfileContainer"));
+
 
 
 class App extends Component {
@@ -33,12 +36,10 @@ if(!this.props.initialized) {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}
+                           render={withSuspense(DialogsContainer) }
                     />
                     <Route path='/profile/:userId?'
-                           render={
-                               () => <ProfileContainer/>
-                           }
+                           render={withSuspense(ProfileContainer) }
                     />
                     <Route path='/users'
                            render={
