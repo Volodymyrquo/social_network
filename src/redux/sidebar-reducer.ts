c25be3a49871866a17b3dcac41cb1e6df636f572
Volodymyr <volodymyrquo@gmail.com>
2020-12-17 09:58:59 +0200

    added typescript to sidebar-reducer

---
+++ src/redux/sidebar-reducer.ts
@@ -0,0 +1,9 @@
+
+type InitialStateType = typeof initialState;
+let initialState = {}
+
+const sidebarReducer = (state = initialState, action:any):InitialStateType => {
+    return state;
+}
+
+export default sidebarReducer;
