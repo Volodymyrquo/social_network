ec013f3d16727ef3fca1ef2899307d4857ab2285
Volodymyr <volodymyrquo@gmail.com>
2021-01-17 18:53:45 +0200

    added typescript to DialogItems

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,31 +1,30 @@
-import React from "react";
-import classes from "./Dialogs.module.css";
-import DialogsItem from "./dialogItem/DialogItem";
-import Message from "./message/Message";
-import AddMessageForm from "../login/AddMessageForm";
-
+import React from 'react';
+import classes from './Dialogs.module.css';
+import DialogsItem from './dialogItem/DialogItem';
+import Message from './message/Message';
+import AddMessageForm from '../login/AddMessageForm';
 
 const Dialogs = (props) => {
+  let dialogsElements = props.dialogs.map((dialog) => (
+    <DialogsItem name={dialog.name} key={dialog.id} id={dialog.id} />
+  ));
+  let messagesElements = props.messages.map((el) => (
+    <Message message={el.message} key={el.id} id={el.id} />
+  ));
 
-    let dialogsElements = props.dialogs.map(dialog => <DialogsItem name={dialog.name} key={dialog.id} id={dialog.id}/>)
-    let messagesElements = props.messages.map(el => <Message message={el.message} key={el.id} id={el.id}/>)
+  const addNewMassage = (values) => {
+    props.addMessage(values.newMessageBody);
+  };
 
-    const addNewMassage = (values) => {
-props.addMessage(values.newMessageBody)
-    }
-
-
-         return (
-        <div className={classes.dialogs}>
-            <div className={classes.dialogsItems}>
-                {dialogsElements} 
-            </div>
-            <div className={classes.messages}>
-                {messagesElements}
-              <AddMessageForm   onSubmit={addNewMassage} />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>{dialogsElements}</div>
+      <div className={classes.messages}>
+        {messagesElements}
+        <AddMessageForm onSubmit={addNewMassage} />
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
--- src/components/dialogs/dialogItem/DialogItem.tsx
+++ src/components/dialogs/dialogItem/DialogItem.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import classes from "./../Dialogs.module.css";
 import {NavLink} from "react-router-dom";
+type PropsType = {
+    name:string;
+      id:number;
+}
+const DialogsItem:React.FC<PropsType> = ({name,id}) => {
 
-const DialogsItem = (props) => {
-
-    let path = '/dialogs/' + props.id;
+    let path = '/dialogs/' + id;
 
     return (
         <div className={classes.dialog + ' ' + classes.active}>
-            <NavLink to={path} activeClassName={classes.activeLink}>{props.name}</NavLink>
+            <NavLink to={path} activeClassName={classes.activeLink}>{name}</NavLink>
         </div>
     );
 }
