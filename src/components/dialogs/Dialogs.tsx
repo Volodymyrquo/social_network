993b321305651bb3e05324ab01b9710c4f43f13c
Volodymyr <volodymyrquo@gmail.com>
2021-01-17 19:32:51 +0200

    added typescript to Dialogs

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,17 +3,24 @@ import classes from './Dialogs.module.css';
 import DialogsItem from './dialogItem/DialogItem';
 import Message from './message/Message';
 import AddMessageForm from '../login/AddMessageForm';
+import { DialogType,MessageType } from "../../redux/dialogs-reducer";
 
-const Dialogs = (props) => {
-  let dialogsElements = props.dialogs.map((dialog) => (
+type PropsType = {
+    dialogs:Array<DialogType>;
+    messages:Array<MessageType>;
+    addMessage:(newMessageBody:string)=>void;
+}
+
+const Dialogs:React.FC<PropsType> = ({dialogs,messages,addMessage}) => {
+  let dialogsElements = dialogs.map((dialog) => (
     <DialogsItem name={dialog.name} key={dialog.id} id={dialog.id} />
   ));
-  let messagesElements = props.messages.map((el) => (
-    <Message message={el.message} key={el.id} id={el.id} />
+  let messagesElements = messages.map((el) => (
+    <Message message={el.message} key={el.id} />
   ));
 
-  const addNewMassage = (values) => {
-    props.addMessage(values.newMessageBody);
+  const addNewMassage = (values:any) => {
+    addMessage(values.newMessageBody);
   };
 
   return (
--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -1,7 +1,7 @@
 const ADD_MESSAGE = 'ADD-MESSAGE';
 
-type DialogType = {id:number; name:string}
-type MessageType = {id:number; message:string}
+export type DialogType = {id:number; name:string}
+export type MessageType = {id:number; message:string}
 export type InitialStateType = typeof initialState;
 
 let initialState = {
